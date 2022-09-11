053dc2ea4e8484becb58047c03149413a3e51bb5
RayMose <yassinmusa17@gmail.com>
2022-09-11 22:13:42 +0300

    Yet another

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,6 +18,6 @@ int main(void)
 	printf("size of a long int: %10 byte(s)\n", sizeof(li));
 	printf("size of a long long int: %10 byte(s)\n", sizeof(lli));
 	printf("size of a float: %10 byte(s)\n" sizeof(f));
-
+	
 	return (0);
 }
