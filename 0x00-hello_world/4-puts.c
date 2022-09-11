27431fee8965349b54056eca4bb0bd57106c61df
RayMose <yassinmusa17@gmail.com>
2022-09-11 11:09:31 -0700

    Yet another correction for the 4 task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
+
 /**
- * main - Entry point
- * 
+ * main - Print the string in the put function
+ *
+ * Description - Write a C program that prints exactly "Programming is like building a multilingual puzzle," followed by a new line.
  * Return:0 after printing the function   
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle."):
+{
+	puts("\"Programming is like building a multilingual puzzle."):
 	    return (0);
 }
