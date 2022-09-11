e05988aae22fc7e5001833e85c806ae13d25d5b9
RayMose <yassinmusa17@gmail.com>
2022-09-11 11:38:55 -0700

    Yet another

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *main - Print the string in the put function
  *
  *Description - Write a C program that prints exactly "Programming is like building a multilingual puzzle," followed by a new line.
- * Return:0 after printing the function
+ *Return:0 after printing the function
  */
 int main(void)
 {
