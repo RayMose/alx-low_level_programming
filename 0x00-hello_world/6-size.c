50c9cdc14e276b03773eb574b0faee15bd8a196a
RayMose <yassinmusa17@gmail.com>
2022-09-11 22:01:36 +0300

    Yet another

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,23 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
 printf("size of a char: %10 byte(s)\n", ()unsigned long)sizeof(d));
-printf("size of an int: %10 byte(s)\n", ()unsigned long)sizeof(d));                                                                    
-printf("size of a char: %10 byte(s)\n", ()unsigned long)sizeof(d));                                                                    
-printf("size of a char: %10 byte(s)\n", ()unsigned long)sizeof(d));                                                                    
-printf("size of a char: %10 byte(s)\n", ()unsigned long)sizeof(d));                                                                    
+printf("size of an int: %10 byte(s)\n", ()unsigned long)sizeof(d));
+printf("size of a char: %10 byte(s)\n", ()unsigned long)sizeof(d));
+printf("size of a char: %10 byte(s)\n", ()unsigned long)sizeof(d));
+printf("size of a char: %10 byte(s)\n" ()unsigned long)sizeof(d));
 
-	    return (0);
+	return (0);
 }
