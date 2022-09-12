e57d03b75be236ffd940d89e7b95b6486c88cf9b
RayMose <yassinmusa17@gmail.com>
2022-09-12 12:13:14 +0300

    Yet another commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,22 +1,18 @@
-
 #include <stdio.h>
 
-/*
- *  * main -Prints the alphabet in reverse
- *   *
- *    *
- *     *Return: Always (Success).
- *
- *      */
+/**
+*main -Prints the alphabet in reverse
+*
+*Return: Always (Success).
+*
+*/
 int main(void)
 {
+	char c;
 
-	    char c;
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
 
-	        for (c = 'z'; n <= 'a'; c--)
-			        putchar(c);
-
-		    putchar('\n');
-
-		        return (0);
+	return (0);
 }
