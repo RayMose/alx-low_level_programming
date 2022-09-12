bdcd5454bfcd7014834b33be7eb94101bb762847
RayMose <yassinmusa17@gmail.com>
2022-09-12 11:51:15 +0300

    Yet another commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,19 @@
-/*
- *  * File: 0-positive_or_negative_c
- *   *
- *    */
-
-
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
-/*
- *  * main -lowercase alphabets
- *   *
- *    *
- *     *Return: Always (Success).
+/**
+ *main -lowercase alphabets
  *
- *      */
+ *Return: Always (Success).
+ *
+ */
 int main(void)
 {
-		int n;
+	char ch;
 
-		    char c;
-
-		    	srand(time(0));
-				n = rand() - RAND_MAX / 2;
-
-					for (c = 'a'; c <= 'z'; c++)
-						       putchar(c);
-					    putchar('\n');
-					        
-					    	return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
 }
