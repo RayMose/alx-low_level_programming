68f637e88f095fdd215ff930a1f917fcc7d3663f
RayMose <yassinmusa17@gmail.com>
2022-09-22 21:34:02 +0300

    Another important commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,15 @@
  * Return: A pointer to the destination string @dest.
  *
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int index = 0; dest_len = 0;
 
 	while (dest[index++])
-		dest_len++
+		dest_len++;
 
-	for (index = 0; src[index] && index < n; index++)
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
-		return (dest);
+
+	return (dest);
 }
