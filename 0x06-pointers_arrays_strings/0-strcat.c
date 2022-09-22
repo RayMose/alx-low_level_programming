80915f4a136e5d59b4e7b4e76e6b8363834b15af
RayMose <yassinmusa17@gmail.com>
2022-09-23 02:34:15 +0300

    Yet another commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
 /**
  * strcat - Concatenates the string to by @src, including the terminat
  *null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
- *
  * Return: A pointer to the destination string @dest.
- *
  */
 char *_strcat(char *dest, char *src)
 {
